cb36f8ba7de9a780c76ddad225830a9c4c12a248
Mostafa Zamanian <zamanian.wisc@gmail.com>
2021-02-21 12:06:23 -0600

    add sc auxillary scripts

---
+++ auxillary/sc_scripts/gtf_process.R
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+library(tidyverse)
+
+gtf <- read.table("geneset.gtf", sep = "\t", header = FALSE, quote = "")
+ext <- as.numeric(args[1])
+
+gtf.3ext <- gtf %>%
+  mutate(V5 = ifelse(V3 == "three_prime_utr" & V7 =="+", as.numeric(V5) + ext, V5)) %>%
+  mutate(V4 = ifelse(V3 == "three_prime_utr" & V7 =="-", as.numeric(V4) - ext, V4)) %>%
+  mutate(V4 = ifelse(V3 == "three_prime_utr" & V4 < 0, 0, V4)) 
+
+write.table(gtf.3ext,"geneset.3ext.gtf", sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
