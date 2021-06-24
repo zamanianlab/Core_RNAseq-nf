df74b28e97c4e1a9b9eb6ea58737148b4d6aaeab
chenthorn <61562303+chenthorn@users.noreply.github.com>
2021-06-24 12:34:58 -0500

    apply ext to final exons

--- auxillary/sc_scripts/gtf_process.R
+++ auxillary/sc_scripts/gtf_process.R
@@ -13,9 +13,19 @@ colnames(contig.len) <- c("seqname","len")
 #read in 3' UTR extension value
 ext <- as.numeric(args[1])
 
-#apply extension
+#apply extension to gene, transcript, last exon, and 3' UTR
 gtf.3ext <- gtf %>%
-  mutate(V5 = ifelse(V3 == "three_prime_utr" & V7 =="+", as.numeric(V5) + ext, V5)) %>%
+  mutate(transcript_id = str_match(V9, "transcript_id \"(.*?)\";")[,2]) %>%
+  mutate(exon_number = str_match(V9, "exon_number \"(\\d+)\";")[,2]) %>%
+  mutate(exon_number = replace_na(exon_number, as.character(0))) %>% 
+  mutate(exon_number = as.numeric(exon_number)) %>% 
+  group_by(transcript_id) %>% 
+  mutate(group_max = max(exon_number)) %>% 
+  mutate(group_max = ifelse(exon_number == group_max & V3 == "exon", "yes", "no")) %>% 
+  ungroup() %>% 
+  mutate(V5 = ifelse(group_max == "yes" & V7 == "+", as.numeric(V5) + ext, V5)) %>% 
+  mutate(V4 = ifelse(group_max == "yes" & V7 == "-", as.numeric(V4) - ext, V4)) %>% 
+  mutate(V4 = ifelse(group_max == "yes" & V4 < 1, 1, V4)) %>%
   mutate(V4 = ifelse(V3 == "three_prime_utr" & V7 =="-", as.numeric(V4) - ext, V4)) %>%
   mutate(V4 = ifelse(V3 == "three_prime_utr" & V4 < 1, 1, V4)) %>%
   mutate(V5 = ifelse(V3 == "transcript" & V7 =="+", as.numeric(V5) + ext, V5)) %>%
@@ -24,7 +34,7 @@ gtf.3ext <- gtf %>%
   mutate(V5 = ifelse(V3 == "gene" & V7 =="+", as.numeric(V5) + ext, V5)) %>%
   mutate(V4 = ifelse(V3 == "gene" & V7 =="-", as.numeric(V4) - ext, V4)) %>%
   mutate(V4 = ifelse(V3 == "gene" & V4 < 1, 1, V4))
-colnames(gtf.3ext) <- c("seqname","source","feature","start","end","score","strand","frame","attribute")
+colnames(gtf.3ext) <- c("seqname","source","feature","start","end","score","strand","frame","attribute", "transcript_id", "exon_number", "group_max")
 
 #truncate if exceeds boundaries of contig 
 gtf.3ext <- left_join(gtf.3ext,contig.len, by = "seqname") %>%
